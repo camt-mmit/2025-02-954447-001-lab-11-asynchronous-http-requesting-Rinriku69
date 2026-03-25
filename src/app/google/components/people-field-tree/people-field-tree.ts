9d7993595931198e7758750e3919353a023cda89
Rinriku69 <siripoohkim@gmail.com>
2026-03-25 21:52:45 +0700

    style: style form

--- src/app/google/components/people-field-tree/people-field-tree.ts
+++ src/app/google/components/people-field-tree/people-field-tree.ts
@@ -12,21 +12,21 @@ import { ContactInsertModel } from '../../helpers';
 export class PeopleFieldTree {
   readonly fieldTree = input.required<FieldTree<ContactInsertModel>>();
 
-  // --- Logic สำหรับจัดการ Array ของ Emails ---
+
   addEmail() {
-    // เข้าถึง WritableSignal ของฟิลด์ emails โดยตรง
+  
     const emailsSignal = this.fieldTree().emails().value;
-    // นำค่าเดิมมาต่อด้วย Object ใหม่
+
     emailsSignal.set([...emailsSignal(), { type: '', value: '' }]);
   }
 
   removeEmail(index: number) {
     const emailsSignal = this.fieldTree().emails().value;
-    // กรองเอาตัวที่ index ตรงกับที่กดลบออก
+
     emailsSignal.set(emailsSignal().filter((_, i) => i !== index));
   }
 
-  // --- Logic สำหรับจัดการ Array ของ Phones ---
+
   addPhone() {
     const phonesSignal = this.fieldTree().phones().value;
     phonesSignal.set([...phonesSignal(), { type: '', value: '' }]);
